d8fdab7672b03f96e030d20073963b82683394f4
Philipp Skadorov <philipp.skadorov@teqniksoft.com>
2016-01-12 17:48:33 +0300

    test for clang-format

--- hello.c
+++ hello.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+void test_format(void)
+{
+    int i;//comment 1;
+    int long_var_name;//comment2
+    int very_very_very_long_var_name;//comment3
+    char* ptr;
+    for(i=0;i<10;i++){printf("%d",i);}
+        char *unaligned = 0;
+
+
+
+    printf("Hello\n");
+    switch(i)
+        {
+            case 1:
+                break;
+    case 2:
+    break;
+        default:
+            break;
+        }
+    int j=5;
+		int k=6;
+}
+
 int main(int argc, char **argv)
 {
     printf("Hello World!\n");
